f2964f3b91fdc6a83fe00f4a9d6f2006e4e4b211
s-nandi <saujas.nandi@gmail.com>
2019-05-19 22:55:56 -0400

    Set up ForcedActionsAgent to return actions based on the list of forced actions it was constructed with

--- marioaiDagstuhl/src/cmatest/ForcedActionsAgent.java
+++ marioaiDagstuhl/src/cmatest/ForcedActionsAgent.java
@@ -6,17 +6,21 @@ import competition.icegic.robin.astar.AStarSimulator;
 import ch.idsia.mario.engine.sprites.Mario;
 import ch.idsia.mario.environments.Environment;
 
+import java.util.ArrayList;
+
 public class ForcedActionsAgent extends BasicAIAgent implements Agent
 {
     protected boolean action[] = new boolean[Environment.numberOfButtons];
-    protected String name = "AStarAgentForcedActions";
+    protected String name = "ForcedActionsAgent";
     private AStarSimulator sim;
     private int tickCounter = 0;
     private float lastX = 0;
     private float lastY = 0;
+    private ArrayList<boolean[]> moves;
 
-    public ForcedActionsAgent() {
+    public ForcedActionsAgent(ArrayList<boolean[]> moves) {
         super("ForcedActionsAgent");
+        this.moves = moves;
     }
 
     public void reset()
@@ -42,68 +46,15 @@ public class ForcedActionsAgent extends BasicAIAgent implements Agent
 
     public boolean[] getAction(Environment observation)
     {
-        tickCounter++;
-        String s = "Fire";
-        if (!sim.levelScene.mario.fire)
-            s = "Large";
-        if (!sim.levelScene.mario.large)
-            s = "Small";
-        if (sim.levelScene.verbose > 0) System.out.println("Next action! Tick " + tickCounter + " Simulated Mariosize: " + s);
-
-        boolean[] ac = new boolean[5];
-        ac[Mario.KEY_RIGHT] = true;
-        ac[Mario.KEY_SPEED] = true;
-
-        //byte[][] scene = observation.getCompleteObservation();//observation.getLevelSceneObservation(0);
-        byte[][] scene = observation.getLevelSceneObservation();
-        float[] enemies = observation.getEnemiesFloatPos();
-
-        //observation.getCompleteObservation();
-        //System.out.println("Clean scene:");
-        //printLevel(scene);
-
-        //System.out.println("Complete Obs:");
-        //printLevel(observation.getCompleteObservation());
-
-        if (sim.levelScene.verbose > 2) System.out.println("Simulating using action: " + sim.printAction(action));
-        sim.advanceStep(action);
-
-        if (sim.levelScene.verbose > 5) System.out.println("Simulated sprites: ");
-        if (sim.levelScene.verbose > 5) sim.levelScene.dumpSprites();
-
-        //System.out.println("Internal scene after sim:");
-        //printLevel(sim.levelScene.levelSceneObservation(0));
-
-        sim.setLevelPart(scene, enemies);
-        //printLevel(sim.levelScene.levelSceneObservation(0));
-        float[] f = observation.getMarioFloatPos();
-        if (sim.levelScene.verbose > 5)
-            System.out.println("Sim Mario Pos: "
-                    + sim.levelScene.mario.x + " " + sim.levelScene.mario.y + " " +
-                    " a: " + sim.levelScene.mario.xa + " " + sim.levelScene.mario.ya );
-        if (sim.levelScene.mario.x != f[0] || sim.levelScene.mario.y != f[1])
+        if (tickCounter < 0)
         {
-            if (f[0] == lastX && f[1] == lastY)
-                return ac;
-            //System.out.print("i");
-            if (sim.levelScene.verbose > 0) System.out.println("INACURATEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!");
-            if (sim.levelScene.verbose > 0) System.out.println("Real: "+f[0]+" "+f[1]
-                    + " Est: "+ sim.levelScene.mario.x + " " + sim.levelScene.mario.y +
-                    " Diff: " + (f[0]- sim.levelScene.mario.x) + " " + (f[1]-sim.levelScene.mario.y));
-            sim.levelScene.mario.x = f[0];
-            sim.levelScene.mario.y = f[1];
-            sim.levelScene.mario.xa = (f[0] - lastX) *0.89f;
-            sim.levelScene.mario.ya = (f[1] - lastY) * 0.85f + 3f;
-            //errCount++;
-            //if (errCount > 1)
-            //	errAgent.lastX++;
+            throw new RuntimeException("The current tick is negative which is invalid");
         }
-        lastX = f[0];
-        lastY = f[1];
+        // If the tickCounter is greater than the number of frames, cycle back to the beginning
+        int frameNumber = tickCounter % moves.size();
+        boolean [] action = moves.get(frameNumber);
+        tickCounter++;
 
-        action = sim.optimise();
-
-        if (sim.levelScene.verbose > 1) System.out.println("Returning action: " + sim.printAction(action));
         return action;
     }
 
